3749414e9a9b57e382b8b98928c60b07bfeadd83
Paul Jreij <pjreij98@gmail.com>
2024-12-26 22:18:26 -0600

    Initial Spring Boot project setup

---
+++ src/main/java/com/local_adventure_backend/LocalAdventureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.local_adventure_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalAdventureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalAdventureBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/local_adventure_backend/LocalAdventureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local_adventure_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalAdventureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
